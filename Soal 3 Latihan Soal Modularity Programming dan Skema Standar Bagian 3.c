9e7e3d121531424f26bd9b7280c83b07f72ab88c
MurtadhaAri <murtadhaarlkari@gmail.com>
2025-03-15 23:44:47 +0700

    Update Soal 3 Latihan Soal Modularity Programming dan Skema Standar Bagian 3.c

--- Soal 3 Latihan Soal Modularity Programming dan Skema Standar Bagian 3.c
+++ Soal 3 Latihan Soal Modularity Programming dan Skema Standar Bagian 3.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int a = 1, b = 2, c = 3, temp;
-    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    int a, b, c, temp;
     
+    // Meminta input dari user
+    printf("Masukkan nilai a: ");
+    scanf("%d", &a);
+    printf("Masukkan nilai b: ");
+    scanf("%d", &b);
+    printf("Masukkan nilai c: ");
+    scanf("%d", &c);
+    
+    printf("Sebelum pertukaran: a = %d, b = %d, c = %d\n", a, b, c);
+    
+    // Menukar nilai a, b, c seperti pada kode awal
     temp = a;
     a = c;
     c = b;
     b = temp;
     
-    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    printf("Setelah pertukaran: a = %d, b = %d, c = %d\n", a, b, c);
     
     return 0;
 }
